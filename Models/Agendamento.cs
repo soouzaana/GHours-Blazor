3fb03624383cc487dca4a446cec9d46a846a1809
Gabriella Osório Ribeiro <gabriellaribeiro0934@gmail.com>
2025-09-29 11:28:34 -0400

    criei agendamento.cs e agendamnetoDAO.cs

---
+++ Models/Agendamento.cs
@@ -0,0 +1,15 @@
+namespace AppExemplo.Models
+{
+    public class Agendamento
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public TimeSpan HoraInicial { get; set; }
+        public TimeSpan HoraFinal { get; set; }
+        public string Status { get; set; }
+        public decimal ValorTotal { get; set; }
+        public DateTime DataReserva { get; set; }
+        public int QuadraId { get; set; }
+    }
+}
+
---
+++ Models/AgendamentoDAO.cs
@@ -0,0 +1,39 @@
+using AppExemplo.Configs;
+
+namespace AppExemplo.Models
+{
+    public class AgendamentoDAO
+    {
+        private readonly Conexao _conexao;
+        public AgendamentoDAO(Conexao conexao)
+        {
+            _conexao = conexao;
+        }
+
+        public List<Agendamento> ListarTodos() { 
+        
+            var lista = new List<Agendamento>();
+
+            var comando = _conexao.CreateCommand("SELECT * FROM agendamento");
+            var leitor = comando.ExecuteReader();
+
+            while(leitor.Read())
+            {
+                var agendamento = new Agendamento
+                {
+                    Id = leitor.GetInt32("id_agen"),
+                    Data = leitor.GetDateTime("data_agen"),
+                    HoraInicial = leitor.GetTimeSpan("horaInicial_agen"),
+                    HoraFinal = leitor.GetTimeSpan("horaFinal_agen"),
+                    Status = leitor.GetString("status_agen"),
+                    ValorTotal = leitor.GetDecimal("valor_total"),
+                    DataReserva = leitor.GetDateTime("data_reserva_agen"),
+                    QuadraId = leitor.GetInt32("id_qua_fk")
+                };
+
+                lista.Add(agendamento);
+            }
+            return lista;
+        }
+    }
+}
