2aa239c32439531cd82d076c73bab986353bd315
Gabriella Osório Ribeiro <gabriellaribeiro0934@gmail.com>
2025-09-29 11:05:44 -0400

    criei a classe cliente e clienteDAO

---
+++ Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace AppExemplo.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Models/ClienteDAO.cs
@@ -0,0 +1,36 @@
+﻿using AppExemplo.Configs;
+
+namespace AppExemplo.Models
+{
+    public class ClienteDAO
+    {
+        private readonly Conexao _conexao;
+        public ClienteDAO(Conexao conexao)
+        {
+            _conexao = conexao;
+        }
+
+        public List<Cliente> ListarTodos() { 
+        
+            var lista = new List<Cliente>();
+
+            var comando = _conexao.CreateCommand("SELECT * FROM cliente");
+            var leitor = comando.ExecuteReader();
+
+            while(leitor.Read())
+            {
+                var cliente = new Cliente
+                {
+                    Id = leitor.GetInt32("id_cli"),
+                    Nome = leitor.GetString("nome_cli"),
+                    Cpf = leitor.GetString("cpf_cli"),
+                    Telefone = leitor.GetString("telefone_cli"),
+                    Email = leitor.GetString("email_cli")
+                };
+
+                lista.Add(cliente);
+            }
+            return lista;
+        }
+    }
+}
