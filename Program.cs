0dd923d970e1275f438cb1b4cc785d7d96890718
Gabriella Osório Ribeiro <gabriellaribeiro0934@gmail.com>
2025-10-06 11:32:58 -0400

    acho que arrumei o conflito

--- Program.cs
+++ Program.cs
@@ -9,14 +9,10 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddSingleton<Conexao>();
-<<<<<<< HEAD
-//builder.Services.AddSingleton<ProdutoDAO>();
-=======
 builder.Services.AddSingleton<AgendamentoDAO>();
 builder.Services.AddSingleton<ClienteDAO>();
 builder.Services.AddSingleton<FuncionarioDAO>();
 builder.Services.AddSingleton<QuadraDAO>();
->>>>>>> f0534cedf7c074d251ead9001e3dd9edd9173b5c
 
 var app = builder.Build();
 
